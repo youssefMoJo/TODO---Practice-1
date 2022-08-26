21bc1b9021c8b83945adb04c60a661119d35ae55
youssefMoJo <youssefrafaat@yahoo.com>
2022-08-26 15:27:43 -0230

    added the delete button :)

--- src/AddNewTodo.js
+++ src/AddNewTodo.js
@@ -5,9 +5,12 @@ import TodoListContainer from "./TodoListContainer"
 
 const AddNewTodo = () => {
     const [todoMessage, setTodoMessage] = useState("")
+    const [todoList, setTodoList] = useState([])
+    let todoListClassStatus = todoList.length > 0 ? "TodoListContainerShow" : "TodoListContainerHide"
 
     const onEnterPress = (e) => {
         if (e.keyCode === 13 && e.shiftKey === false) {
+            setTodoList(() => [...todoList, todoMessage])
             setTodoMessage("")
             e.preventDefault();
         }
@@ -17,6 +20,10 @@ const AddNewTodo = () => {
         setTodoMessage(event.target.value);
     };
 
+    const removeTodoElementId = (id) => {
+        todoList.splice(id, 1)
+        setTodoList([...todoList])
+    }
 
     return (
         <div>
@@ -26,10 +33,15 @@ const AddNewTodo = () => {
                 onKeyDown={onEnterPress}
                 value={todoMessage}
                 onChange={handleMessageChange}
+                maxLength="50"
             >
             </textarea>
             <GradientBG />
-            <TodoListContainer />
+            <TodoListContainer
+                todoListContainerStatus={todoListClassStatus}
+                todoList={todoList}
+                removeTodoElementId={removeTodoElementId}
+            />
         </div>
     )
 }
--- src/TodoListContainer.js
+++ src/TodoListContainer.js
@@ -1,9 +1,39 @@
+import { useState } from 'react';
 import './TodoListContainer.css';
 
-const TodoListContainer = () => {
-    return (
+const TodoListContainer = (props) => {
+    let todoList = props.todoList
+    const [deleteButtonStatus, setDeleteButtonStatus] = useState("deleteButtonHide")
 
-        <div className="TodoListContainer">
+    const removeTodoElementId = (key) => {
+        props.removeTodoElementId(key)
+    }
+    const showTheDeleteButton = (status) => {
+        if (status === "enter") {
+            setDeleteButtonStatus("deleteButtonShow")
+        } else {
+            setDeleteButtonStatus("deleteButtonHide")
+        }
+    }
+
+    return (
+        <div className={props.todoListContainerStatus}>
+
+            {todoList.map((todoElement, key) => {
+                return <div
+                    onMouseEnter={() => showTheDeleteButton("enter")}
+                    onMouseLeave={() => showTheDeleteButton("leave")}
+                    className="todoElement"
+                    // onClick={() => removeTodoElementId(key)}
+                    key={key}
+                >
+                    <input type="checkbox" id="ossm" name="ossm" />
+                    <label > {todoElement}</label>
+                    <div id={deleteButtonStatus} className="deleteTodoButton">
+                        X
+                    </div>
+                </div>
+            })}
 
         </div>
     )
